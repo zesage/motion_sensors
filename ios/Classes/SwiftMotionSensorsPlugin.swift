54867c3adecd2bcc88d6977e6fdbbf11725ee36d
Mark Hu <zesage@gmail.com>
2020-08-16 16:09:21 +0800

    fix sensor not working on iOS

--- ios/Classes/SwiftMotionSensorsPlugin.swift
+++ ios/Classes/SwiftMotionSensorsPlugin.swift
@@ -3,7 +3,6 @@ import UIKit
 import CoreMotion
 
 let GRAVITY = 9.8;
-let motionManager = CMMotionManager();
 
 // translate from https://github.com/flutter/plugins/tree/master/packages/sensors
 public class SwiftMotionSensorsPlugin: NSObject, FlutterPlugin {
@@ -13,16 +12,16 @@ public class SwiftMotionSensorsPlugin: NSObject, FlutterPlugin {
         let USER_ACCELEROMETER_CHANNEL_NAME = "final.dev/plugins/motion_sensors/user_accel"
         let MAGNETOMETER_CHANNEL_NAME = "final.dev/plugins/motion_sensors/magnetometer"
         let ORIENTATION_CHANNEL_NAME = "final.dev/plugins/motion_sensors/orientation"
-
+        
         let accelerometerChannel = FlutterEventChannel(name: ACCELEROMETER_CHANNEL_NAME, binaryMessenger: registrar.messenger());
         accelerometerChannel.setStreamHandler(AccelerometerStreamHandler());
-
+        
         let userAccelerometerChannel = FlutterEventChannel(name: USER_ACCELEROMETER_CHANNEL_NAME, binaryMessenger: registrar.messenger());
         userAccelerometerChannel.setStreamHandler(UserAccelerometerStreamHandler());
-
+        
         let gyroscopeChannel = FlutterEventChannel(name: GYROSCOPE_CHANNEL_NAME, binaryMessenger: registrar.messenger());
         gyroscopeChannel.setStreamHandler(GyroscopeStreamHandler());
-
+        
         let magnetometerChannel = FlutterEventChannel(name: MAGNETOMETER_CHANNEL_NAME, binaryMessenger: registrar.messenger());
         magnetometerChannel.setStreamHandler(MagnetometerStreamHandler());
         
@@ -32,15 +31,18 @@ public class SwiftMotionSensorsPlugin: NSObject, FlutterPlugin {
 }
 
 class AccelerometerStreamHandler: NSObject, FlutterStreamHandler {
+    private let motionManager = CMMotionManager();
     private let queue = OperationQueue();
-
+    
     func onListen(withArguments arguments: Any?, eventSink events: @escaping FlutterEventSink) -> FlutterError? {
-        motionManager.startAccelerometerUpdates(to: queue) { (data, error) in
-            events([-data!.acceleration.x * GRAVITY, -data!.acceleration.y * GRAVITY, -data!.acceleration.z * GRAVITY]);
+        if motionManager.isAccelerometerAvailable {
+            motionManager.startAccelerometerUpdates(to: queue) { (data, error) in
+                events([-data!.acceleration.x * GRAVITY, -data!.acceleration.y * GRAVITY, -data!.acceleration.z * GRAVITY]);
+            }
         }
         return nil;
     }
-
+    
     func onCancel(withArguments arguments: Any?) -> FlutterError? {
         motionManager.stopAccelerometerUpdates();
         return nil;
@@ -48,15 +50,18 @@ class AccelerometerStreamHandler: NSObject, FlutterStreamHandler {
 }
 
 class UserAccelerometerStreamHandler: NSObject, FlutterStreamHandler {
+    private let motionManager = CMMotionManager();
     private let queue = OperationQueue();
-
+    
     func onListen(withArguments arguments: Any?, eventSink events: @escaping FlutterEventSink) -> FlutterError? {
-        motionManager.startDeviceMotionUpdates(to: queue) { (data, error) in
-            events([-data!.userAcceleration.x * GRAVITY, -data!.userAcceleration.y * GRAVITY, -data!.userAcceleration.z * GRAVITY]);
+        if motionManager.isDeviceMotionAvailable {
+            motionManager.startDeviceMotionUpdates(to: queue) { (data, error) in
+                events([-data!.userAcceleration.x * GRAVITY, -data!.userAcceleration.y * GRAVITY, -data!.userAcceleration.z * GRAVITY]);
+            }
         }
         return nil;
     }
-
+    
     func onCancel(withArguments arguments: Any?) -> FlutterError? {
         motionManager.stopDeviceMotionUpdates();
         return nil;
@@ -64,15 +69,18 @@ class UserAccelerometerStreamHandler: NSObject, FlutterStreamHandler {
 }
 
 class GyroscopeStreamHandler: NSObject, FlutterStreamHandler {
+    private let motionManager = CMMotionManager();
     private let queue = OperationQueue();
-
+    
     func onListen(withArguments arguments: Any?, eventSink events: @escaping FlutterEventSink) -> FlutterError? {
-        motionManager.startGyroUpdates(to: queue) { (data, error) in
-            events([data!.rotationRate.x, data!.rotationRate.y, data!.rotationRate.z]);
+        if motionManager.isGyroAvailable {
+            motionManager.startGyroUpdates(to: queue) { (data, error) in
+                events([data!.rotationRate.x, data!.rotationRate.y, data!.rotationRate.z]);
+            }
         }
         return nil;
     }
-
+    
     func onCancel(withArguments arguments: Any?) -> FlutterError? {
         motionManager.stopGyroUpdates();
         return nil;
@@ -80,8 +88,9 @@ class GyroscopeStreamHandler: NSObject, FlutterStreamHandler {
 }
 
 class MagnetometerStreamHandler: NSObject, FlutterStreamHandler {
+    private let motionManager = CMMotionManager();
     private let queue = OperationQueue();
-
+    
     func onListen(withArguments arguments: Any?, eventSink events: @escaping FlutterEventSink) -> FlutterError? {
         if motionManager.isDeviceMotionAvailable {
             motionManager.showsDeviceMovementDisplay = true;
@@ -91,7 +100,7 @@ class MagnetometerStreamHandler: NSObject, FlutterStreamHandler {
         }
         return nil;
     }
-
+    
     func onCancel(withArguments arguments: Any?) -> FlutterError? {
         motionManager.stopDeviceMotionUpdates()
         return nil;
@@ -99,10 +108,12 @@ class MagnetometerStreamHandler: NSObject, FlutterStreamHandler {
 }
 
 class AttitudeStreamHandler: NSObject, FlutterStreamHandler {
+    private let motionManager = CMMotionManager();
     private let queue = OperationQueue();
     
     func onListen(withArguments arguments: Any?, eventSink events: @escaping FlutterEventSink) -> FlutterError? {
         if motionManager.isDeviceMotionAvailable {
+            motionManager.showsDeviceMovementDisplay = true;
             motionManager.startDeviceMotionUpdates(using: CMAttitudeReferenceFrame.xMagneticNorthZVertical, to: queue) { (data, error) in
                 events([-data!.attitude.yaw, -data!.attitude.pitch, data!.attitude.roll]);
             }
